b79a445deecf3837d3f34b837d8452e7363cddad
Elain520 <2062925063@qq.com>
2025-04-14 14:28:36 +0800

    golang1

--- test_golang.sh
+++ test_golang.sh
@@ -1,10 +1,9 @@
 echo "Start tracing"
 trace-cmd start -p function  # 开始跟踪
 
-# 这里是需要重点跟踪的代码
-# sleep 1
 time bash mugen.sh -f smoke -r oe_test_golang -x
-#
+
 echo "Stop tracing"
 trace-cmd stop  # 停止跟踪
-trace-cmd report  # 生成报告
+trace-cmd extract -o trace.dat
+trace-cmd | awk '{print $5}' > report_func_list.log
