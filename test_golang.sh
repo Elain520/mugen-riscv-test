c8066671f0dc8ef9b679cb56a99dd47ce05269e9
Elain520 <2062925063@qq.com>
2025-04-14 18:00:52 +0800

    openblas1

--- test_golang.sh
+++ test_golang.sh
@@ -2,7 +2,8 @@ echo "Start tracing"
 trace-cmd start -p function  # 开始跟踪
 
 # time bash mugen.sh -f smoke -r oe_test_golang -x
-bash mugen.sh -f python3 -x
+# bash mugen.sh -f python3 -x
+bash mugen.sh -f openblas -x
 
 echo "Stop tracing"
 trace-cmd stop  # 停止跟踪
