05e8680b3712940ea1f2159320546b98eeddc9c6
Elain520 <2062925063@qq.com>
2025-04-15 10:08:16 +0800

    protobuf1

--- test_golang.sh
+++ test_golang.sh
@@ -3,7 +3,8 @@ trace-cmd start -p function  # 开始跟踪
 
 # time bash mugen.sh -f smoke -r oe_test_golang -x
 # bash mugen.sh -f python3 -x
-bash mugen.sh -f openblas -x
+# bash mugen.sh -f openblas -x
+bash mugen.sh -f protobuf -x
 
 echo "Stop tracing"
 trace-cmd stop  # 停止跟踪
