2f844c335e2b964a5a40bcaa1e93c12fd47baa0d
Elain520 <2062925063@qq.com>
2025-04-15 14:30:10 +0800

    memcache1

--- test_golang.sh
+++ test_golang.sh
@@ -4,7 +4,8 @@ trace-cmd start -p function  # 开始跟踪
 # time bash mugen.sh -f smoke -r oe_test_golang -x
 # bash mugen.sh -f python3 -x
 # bash mugen.sh -f openblas -x
-bash mugen.sh -f protobuf -x
+# bash mugen.sh -f protobuf -x
+bash mugen.sh -f memcached -x
 
 echo "Stop tracing"
 trace-cmd stop  # 停止跟踪
